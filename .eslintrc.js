ee1a77a3273878ddb0b54ad925fa34461f983c04
Flavio Kreis <flaviokreis@gmail.com>
2019-11-09 14:38:04 -0300

    Start project and instal navigation

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        'prettier',
+        'prettier/react'
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "prettier"
+    ],
+    "rules": {
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': [
+          'warn',
+          { extensions: ['.jsx', '.js'] }
+      ],
+      'react/prefer-stateless-function': 'off',
+      'import/prefer-default-export': 'off',
+      'react/state-in-constructor': 'off',
+      'react/static-property-placement': 'off',
+      'no-param-reassign': 'off',
+      'no-console': ["error", { allow: ["tron"] }]
+    }
+};
